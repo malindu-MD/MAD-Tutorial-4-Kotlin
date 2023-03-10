4e2b5f5dd61d823fd81439438e9198f92e1699b9
malindu-MD <it21173790@my.sliit.lk>
2023-03-10 23:06:58 +0530

    commit

---
+++ app/src/main/java/com/malindu/mad_tutorial4/models/ValidationResult.kt
@@ -0,0 +1,10 @@
+package com.malindu.mad_tutorial4.models
+
+sealed class ValidationResult{
+
+    data class Empty(val errorMessage:String):ValidationResult()
+    data class Invalid(val errorMessage: String):ValidationResult()
+    object Valid:ValidationResult()
+
+
+}
